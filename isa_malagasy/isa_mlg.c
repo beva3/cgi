e05b7153e71abde78b4318ad7b589eb638988269
beva3 <mizabienvenu@gmail.com>
2023-12-23 06:54:38 +0300

    10 - 90

--- isa_malagasy/isa_mlg.c
+++ isa_malagasy/isa_mlg.c
@@ -5,9 +5,17 @@ char* manisa(int i){
     const char* isa[] = {
         "ray","roa","telo","efatra","dimy","enina","fito","valo","sivy"
     };
+    const char* ampolo[] = {
+        "folo","roapolo","telopolo","efapolo","dimapolo","enimpolo","fitopolo","valopolo","sivifolo"
+    };
     if(i<10)
         return (char*) isa[i-1];
+
+    else if (i >=10 && i <100){
+        if(i%10 == 0)
+            return (char*)ampolo[i/10 -1];
+    }
+    
     else
         return "NULL";
 }
-
--- isa_malagasy/isa_mlg_main.c
+++ isa_malagasy/isa_mlg_main.c
@@ -1,6 +1,7 @@
 #include "isa_mlg.h"
 
 int main(){
-	printf("%s\n",manisa(9));
+	for(int i = 1 ; i<=9 ; i++)
+		printf("%s\n",manisa(i*10));
 	return 0;
 }
